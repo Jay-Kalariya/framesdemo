9779d56e2e8099e8872da057ffeea0289176468e
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-06-30 13:37:35 +0530

    ds

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 import { useEffect } from "react";
 import "./Home.scss";
-import AboutUsEstablishment from "@/components/aboutUsEstablishment/aboutUsEstablishment";
-import AboutUsVision from "@/components/aboutUsVision/aboutUsVision";
-import AboutUsWhyUs from "@/components/aboutUsWhyUs/aboutUsWhyUs";
-import Form from "@/components/forms/form1/Form";
-import SocialPage from "@/components/socialPage/socialpage";
-import SocialMediaImg from "@/images/socials-image.png";
-import AboutUsDemo from "@/components/aboutdemohero/page";
-import HomeHeroSection from "@/components/homeHeroSection/page";
-import AboutUsHero from "@/components/aboutUsHero/aboutUsHero";
+// import AboutUsEstablishment from "@/components/aboutUsEstablishment/aboutUsEstablishment";
+// import AboutUsVision from "@/components/aboutUsVision/aboutUsVision";
+// import AboutUsWhyUs from "@/components/aboutUsWhyUs/aboutUsWhyUs";
+// import Form from "@/components/forms/form1/Form";
+// import SocialPage from "@/components/socialPage/socialpage";
+// import SocialMediaImg from "@/images/socials-image.png";
+// import AboutUsDemo from "@/components/aboutdemohero/page";
+// import HomeHeroSection from "@/components/homeHeroSection/page";
+// import AboutUsHero from "@/components/aboutUsHero/aboutUsHero";
 const AboutUs = () => {
   useEffect(() => {
     document.title = "About Us | Royal Crown";
@@ -17,11 +17,11 @@ const AboutUs = () => {
   return (
     <div>
       {/* <AboutUsDemo /> */}
-      <AboutUsHero />
+      {/* <AboutUsHero /> */}
       {/* <HomeHeroSection/> */}
-      <AboutUsEstablishment />
-      <AboutUsVision />
-      <AboutUsWhyUs
+      {/* <AboutUsEstablishment /> */}
+      {/* <AboutUsVision /> */}
+      {/* <AboutUsWhyUs
         heading={"Why Us"}
         cardOneText={
           "We are known for providing high density laminates with unparalleled services in terms of quality and reliability. We are known for providing high density laminates with unparalleled services in terms of quality and reliability."
@@ -35,11 +35,11 @@ const AboutUs = () => {
         cardFourText={
           "We are known for providing high density laminates with unparalleled services in terms of quality and reliability. We are known for providing high density laminates with unparalleled services in terms of quality and reliability."
         }
-      />
-      <div className="Form_Page">
+      /> */}
+      {/* <div className="Form_Page">
         <SocialPage socialMediaImgSrc={SocialMediaImg} />
         <Form />
-      </div>
+      </div> */}
     </div>
   );
 };
